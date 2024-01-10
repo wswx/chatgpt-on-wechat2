c6d2be1e1fcdf111d4090d43d17d3ad69d6cf32a
wswx <35282496+wswx@users.noreply.github.com>
2024-01-10 17:46:19 +0800

    Add files via upload

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,9 +1,1 @@
-from .event import *
-from .plugin import *
-from .plugin_manager import PluginManager
-
-instance = PluginManager()
-
-register = instance.register
-# load_plugins                = instance.load_plugins
-# emit_event                  = instance.emit_event
+from .feishudoc import *
