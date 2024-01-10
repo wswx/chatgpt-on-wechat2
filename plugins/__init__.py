53b0dc26def0e9457375d19d17f7b592342cc414
wswx <35282496+wswx@users.noreply.github.com>
2024-01-10 17:49:23 +0800

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,1 +1,9 @@
-from .feishudoc import *
+from .event import *
+from .plugin import *
+from .plugin_manager import PluginManager
+
+instance = PluginManager()
+
+register = instance.register
+# load_plugins                = instance.load_plugins
+# emit_event                  = instance.emit_event
